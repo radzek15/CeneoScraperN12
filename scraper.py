67ba9d6f609ab7ac486b08aba2ba6068fd837982
radzek15 <radzek15@wp.pl>
2021-06-26 10:56:59 +0200

    2021-06-26 RP

--- scraper.py
+++ scraper.py
@@ -1,14 +1,13 @@
+from bs4 import BeautifulSoup
 import requests
 import json
-from bs4 import BeautifulSoup
-from requests.models import encode_multipart_formdata
 
 def get_feature(dom_tree, selector, attribute=None):
     try:
         if isinstance(attribute,str):
-             return dom_tree.select_one(selector)[attribute].strip()
+            return dom_tree.select_one(selector)[attribute].strip()
         if isinstance(attribute, list):
-            return[element.text.strip() for element in dom_tree.select(selector)]     
+            return [element.text.strip() for element in dom_tree.select(selector)]
         return dom_tree.select_one(selector).text.strip()
     except (AttributeError, TypeError):
         return None
@@ -28,27 +27,27 @@ features = {
 }
 
 product_id = input("Podaj kod produktu: ")
-next_page = "https://www.ceneo.pl/{}#tab=reviews".format(product_id) 
-all_opinions = []  
-     
+next_page = "https://www.ceneo.pl/{}#tab=reviews".format(product_id)
+all_opinions = []
+
 while next_page:
-    r = requests.get(next_page)
-#r = requests.get ('https://www.ceneo.pl/72542823#tab=reviews')
-    page_dom = BeautifulSoup(r.text,"html.parser")
+    respons = requests.get(next_page)
+    page_dom = BeautifulSoup(respons.text, "html.parser")
     opinions = page_dom.select("div.js_product-review")
+
     for opinion in opinions:
         single_opinion = {key:get_feature(opinion,*value) for key, value in features.items()}
-        single_opinion["opinion_id"] = opinion["data-entry-id"]
+        single_opinion["opinion_id"] = opinion["data-entry-id"],
         all_opinions.append(single_opinion)
 
-    try:    
-       next_page = 'https://www.ceneo.pl'+ \
-       get_feature(page_dom, "a.pagination__next", "href")   
+    try:
+        next_page = 'https://www.ceneo.pl' + \
+            get_feature(page_dom, "a.pagination__next", "href")
     except TypeError:
-       next_page = None
-    print(next_page)    
+        next_page = None 
+    print(next_page)
 
 with open("opinions/{}.json".format(product_id), "w", encoding="UTF-8") as jf:
-     json.dump(all_opinions, jf, indent=4, ensure_ascii=False)    
+    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
 
-# print(json.dump(single_opinion, indent=4, ensure_ascii=False)) 
+# print(json.dumps(all_opinions, indent=4, ensure_ascii=False))
